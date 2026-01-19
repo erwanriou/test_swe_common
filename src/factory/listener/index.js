d06de3f5a7e52be7f1384c38b0957023a0801116
erwanriou <rr.1.riou@gmail.com>
2026-01-19 01:04:45 +0100

    [Library] - update version

--- src/factory/listener/index.js
+++ src/factory/listener/index.js
@@ -1,4 +1,4 @@
-const { consumerOpts } = require("nats")
+const { AckPolicy, DeliverPolicy } = require("nats")
 
 class Listener {
   subject
@@ -14,27 +14,63 @@ class Listener {
   }
 
   async listen() {
+    const nc = this._nats.client()
     const js = this._nats.js()
+    const jsm = await nc.jetstreamManager()
 
-    const durableName = `${this.queueGroupName}-${this.subject}`
-    const deliverSubject = `${durableName}.deliver`
+    const stream = "EVENTS"
+    const durable = `${this.queueGroupName}-${this.subject}`
 
-    const opts = consumerOpts()
-    opts.durable(durableName)
-    opts.deliverTo(deliverSubject)
-    opts.deliverGroup(this.queueGroupName)
-    opts.manualAck()
-    opts.ackWait(this._ackWait)
-    opts.deliverAll()
-    opts.filterSubject(this.subject)
+    console.log(`[Listener] boot`)
+    console.log(`[Listener] subject=${this.subject}`)
+    console.log(`[Listener] queueGroup=${this.queueGroupName}`)
+    console.log(`[Listener] durable=${durable}`)
+    console.log(`[Listener] stream=${stream}`)
 
-    const sub = await js.subscribe(this.subject, opts)
+    // ensure consumer
+    try {
+      const info = await jsm.consumers.info(stream, durable)
+      console.log(`[Listener] consumer exists`, {
+        delivered: info.delivered?.consumer_seq,
+        ackFloor: info.ack_floor?.consumer_seq
+      })
+    } catch {
+      console.log(`[Listener] creating consumer`)
+      await jsm.consumers.add(stream, {
+        durable_name: durable,
+        ack_policy: AckPolicy.Explicit,
+        deliver_policy: DeliverPolicy.All,
+        ack_wait: this._ackWait * 1_000_000,
+        filter_subject: this.subject
+      })
+      console.log(`[Listener] consumer created`)
+    }
 
-    sub.callback(async (err, msg) => {
-      if (err) return
-      const data = this.parseMessage(msg)
-      await this.onMessage(data, msg)
-      msg.ack()
+    console.log(`[Listener] subscribing (PULL)`)
+
+    const sub = await js.pullSubscribe(this.subject, { durable })
+
+    ;(async () => {
+      for (;;) {
+        console.log(`[Listener] fetch…`)
+        const msgs = await sub.fetch(10, { expires: 1000 })
+
+        let count = 0
+        for (const msg of msgs) {
+          count++
+          console.log(`[Listener] msg received`, `subject=${msg.subject}`, `seq=${msg.seq}`)
+
+          const data = this.parseMessage(msg)
+          await this.onMessage(data, msg)
+          msg.ack()
+        }
+
+        if (count === 0) {
+          console.log(`[Listener] no messages`)
+        }
+      }
+    })().catch(err => {
+      console.error(`[Listener] crashed`, err)
     })
   }
 }
